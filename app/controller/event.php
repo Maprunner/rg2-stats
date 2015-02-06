f9ed7aadde6bad0f0271b9a903b5ef93debdde54
Maprunner <simon@maprunner.co.uk>
2015-02-06 10:25:28 +0000

    Add RSS Feed

--- app/controller/event.php
+++ app/controller/event.php
@@ -9,9 +9,8 @@ public function __construct() {
 /*
 * display a list of events
 */
-public function getEventList() {
+public function getEventList($f3) {
 
-  $f3 = \Base::instance();
   $db_file = $f3->get('db_file');
   $db_type = $f3->get('db_type');
   $db = new DB\SQL($db_type.':'.$db_file);
@@ -22,13 +21,7 @@ public function getEventList() {
   $event->link = 'SELECT link FROM sites WHERE events.siteid=sites.id';
   $event->country = 'SELECT country FROM sites WHERE events.siteid=sites.id';
        
-  $event->load(
-    array(),
-    array(
-        'order'=>'date DESC',
-        'limit'=>100
-    )
-  );
+  $event->load(array(), array('order'=>'date DESC', 'limit'=>100));
   
   $eventdata = array();
   $ev = array();
@@ -50,6 +43,37 @@ public function getEventList() {
   $f3->set('content','app/template/events.htm');
   echo Template::instance()->render('/app/template/page.htm');
 
+}
+
+
+public function getEventRSS($f3) {
+  $db_file = $f3->get('db_file');
+  $db_type = $f3->get('db_type');
+  $db = new DB\SQL($db_type.':'.$db_file);
+  $event = new DB\SQL\Mapper($db, 'events');
+  $event->club = 'SELECT club FROM sites WHERE events.siteid=sites.id';
+  $event->link = 'SELECT link FROM sites WHERE events.siteid=sites.id';
+  $event->country = 'SELECT country FROM sites WHERE events.siteid=sites.id';
+  $event->load(array(), array('order'=>'date DESC', 'limit'=>10));
+  
+  $eventdata = array();
+  $ev = array();
+  while (!$event->dry()) {
+    $ev['date'] = $event->date;
+    $ev['name'] = $event->name;
+    //$ev['country'] = $event->country;
+    $ev['eventid'] = $event->eventid;
+    $ev['link'] = $event->link;
+    //$ev['results'] = $event->results;
+    //$ev['courses'] = $event->courses;
+    //$ev['routes'] = $event->routes;
+    $ev['club'] = $event->club;
+    array_push($eventdata, $ev);
+    $event->next();
+  }  
+  $f3->set('events', $eventdata);
+
+  echo Template::instance()->render('app/template/events.rss', 'application/rss+xml');
 }
 
 }
